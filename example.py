3be6b9685ead17c78d297160ac20b924845a36b4
matt-mekha <mtmekha8@gmail.com>
2020-03-23 16:35:16 -0500

    working proof of concept (run example.py)

---
+++ example.py
@@ -0,0 +1,28 @@
+import super_csv
+
+def usa_date_range():
+    dataset.index("location", lambda a,b: a<b)
+    result = dataset.query({
+        "location": {
+            "eq": "United States"
+        },
+        "date": {
+            "gt": "2020-02-29",
+            "lt": "2020-03-16",
+            "comparison": lambda a,b: a<b
+        }
+    })
+    result.print_data()
+
+def more_than_10000_cases():
+    dataset.index("total_cases")
+    result = dataset.query({
+        "total_cases": {
+            "gt": "10000"
+        }
+    })
+    result.print_data()
+
+
+dataset = super_csv.open_csv("example_data/coronavirus_data.csv")
+usa_date_range()
--- super_csv.py
+++ super_csv.py
@@ -1,16 +1,13 @@
 import sys
+import math
 
 class Dataset:
     def __init__(self):
         self.data = []
-
-    def top_row(self, row):
-        self.column_names = row
+        self.indexed_column = ""
+        self.indexed_comparison = lambda a, b: float(a) < float(b)
     
-    def add_row(self, row):
-        self.data.append(row)
-    
-    def index(self, column_name, comparison=(lambda a, b: float(a) < float(b))):
+    def index(self, column_name, comparison = lambda a, b: float(a) < float(b)):
         def quick_sort(column, low, high, comparison): 
             if low < high: 
                 p = partition(column, low, high, comparison)
@@ -33,18 +30,80 @@ class Dataset:
         
         sys.setrecursionlimit(10000)
         quick_sort(self.column_names.index(column_name), 0, len(self.data)-1, comparison)
+        self.indexed_column = column_name
+        self.indexed_comparison = comparison
+    
+    def query(self, query_object):
 
-    def query_one(self, query_object):
-        return []
-    
-    def query_all(self, query_object):
-        return []
-    
-    def delete_one(self, query_object):
-        return
-    
-    def delete_all(self, query_object):
-        return
+        def binary_search(key, conditions):
+
+            def binary_edge_search(low, high, comparison, direction):
+                if low + 1 == high:
+                    return high
+                lower_middle_index = math.floor((high+low)/2)
+                upper_middle_index = math.ceil((high+low)/2)
+                median = self.data[upper_middle_index][key]
+                if comparison(median) == direction:
+                    return binary_edge_search(lower_middle_index, high, comparison, direction)
+                else:
+                    return binary_edge_search(low, upper_middle_index, comparison, direction)
+
+            lowest = 0
+            highest = len(self.data) - 1
+
+            low_edge = lowest
+            high_edge = highest
+            if "eq" in conditions.keys():
+                low_edge = binary_edge_search(lowest, highest, lambda a : self.indexed_comparison(a, conditions["eq"]), True)
+                high_edge = binary_edge_search(lowest, highest, lambda a : self.indexed_comparison(conditions["eq"], a), False)
+            else:
+                if "gt" in conditions.keys():
+                    low_edge = binary_edge_search(lowest, highest, lambda a : self.indexed_comparison(conditions["gt"], a), False)
+                if "lt" in conditions.keys():
+                    high_edge = binary_edge_search(lowest, highest, lambda a : self.indexed_comparison(a, conditions["lt"]), True)
+            
+            if(high_edge < low_edge):
+                error_message("invalid bounds")
+                return []
+
+            return self.data[low_edge:high_edge]
+        
+
+        result_data = self.data
+
+        if self.indexed_column in query_object:
+            result_data = binary_search(self.column_names.index(self.indexed_column), query_object[self.indexed_column])
+            del query_object[self.indexed_column]
+        
+        deletions = []
+        for i, row in enumerate(result_data):
+            for column_name, operations in query_object.items():
+                column_id = self.column_names.index(column_name)
+                for operator, value in operations.items():
+                    if operator == "comparison":
+                        continue
+                    elif operator == "eq":
+                        if not row[column_id] == value:
+                            deletions.append(i)
+                            break
+                    elif operator == "lt":
+                        if not operations["comparison"](row[column_id], value):
+                            deletions.append(i)
+                            break
+                    elif operator == "gt":
+                        if not operations["comparison"](value, row[column_id]):
+                            deletions.append(i)
+                            break
+                if i in deletions:
+                    break
+        
+        for i in range(len(deletions)-1, -1, -1):
+            del result_data[deletions[i]]
+
+        result = Dataset()
+        result.data = result_data
+        result.column_names = self.column_names
+        return result
     
     def print_data(self):
         for row in self.data:
@@ -52,20 +111,17 @@ class Dataset:
 
 
 def open_csv(filepath, delimiter=","):
-    csv_file = open(filepath, "r")
     dataset = Dataset()
+    csv_file = open(filepath, "r")
     for num, line in enumerate(csv_file):
         if line == "":
             continue
-        fields = line.strip().split(delimiter)
+        row = line.strip().split(delimiter)
         if num == 0:
-            dataset.top_row(fields)
+            dataset.column_names = row
         else:
-            dataset.add_row(fields)
+            dataset.data.append(row)
     return dataset
 
-dataset = open_csv("example_data/coronavirus_data.csv")
-#dataset.index("location", lambda a, b: a < b)
-#dataset.index("date", lambda a, b: a < b)
-dataset.index("total_cases")
-dataset.print_data()
+def error_message(msg):
+    print("[super_csv] ERROR: "+msg)
