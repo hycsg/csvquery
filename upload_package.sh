b0a5da0a9ab7e7b212305450c7f322c463e29310
matt-mekha <mtmekha8@gmail.com>
2020-03-23 22:23:19 -0500

    not testpy

--- upload_package.sh
+++ upload_package.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp README.md package/README.md
 cp csv_query.py package/csv-query/
-cd package && python3 setup.py sdist bdist_wheel && python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+cd package && python3 setup.py sdist bdist_wheel && python3 -m twine upload dist/*
