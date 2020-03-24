73dc49bd6fbfac6c83d6172f53df7115d56f8aa3
matt-mekha <mtmekha8@gmail.com>
2020-03-23 22:21:31 -0500

    added upload command

---
+++ upload_package.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cp README.md package/README.md
+cp csv_query.py package/csv-query/
+cd package && python3 setup.py sdist bdist_wheel && python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
