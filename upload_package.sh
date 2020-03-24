2749fca53a4b3e32cc15bc558cc3b15e9428734d
matt-mekha <mtmekha8@gmail.com>
2020-03-23 23:01:39 -0500

    updated shell file

--- upload_package.sh
+++ upload_package.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp README.md package/README.md
 cp csv_query.py package/csv-query/
-cd package && python3 setup.py sdist bdist_wheel && python3 -m twine upload dist/*
+cd package && rm -r dist && python3 setup.py sdist bdist_wheel && python3 -m twine upload dist/*
