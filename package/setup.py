94d5a0aba045152b29fbc789a53b862cf4ad0f73
matt-mekha <mtmekha8@gmail.com>
2020-03-23 22:05:05 -0500

    prepared package

---
+++ package/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="csv-query",
+    version="0.1.0",
+    author="Houston Youth Computer Science Group",
+    author_email="houstoncsgroup@gmail.com",
+    description="A python package that allows you to execute NoSQL-style queries on CSV files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://https://github.com/Houston-Youth-Computer-Science-Group/csv-query",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
