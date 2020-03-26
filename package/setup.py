a6941b7cccfca8b1f02094cf342b4f5fe0b48622
matt-mekha <mtmekha8@gmail.com>
2020-03-26 16:57:23 -0500

    dependencies

--- package/setup.py
+++ package/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="csvquery",
-    version="1.0.0",
+    version="1.0.1",
     author="Houston Youth Computer Science Group",
     author_email="houstoncsgroup@gmail.com",
     description="A versatile python package that allows you to execute MongoDB-style queries on CSV files.",
@@ -17,6 +17,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests',
     ],
     python_requires='>=3.4',
 )
