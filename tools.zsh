698e2b2ce8afc889d11b14533b242e90247bd4a5
AlexDvorak <doe.jimmie@gmail.com>
2020-03-28 17:25:44 -0500

    tools reflects new structure of package

--- tools.zsh
+++ tools.zsh
@@ -7,24 +7,21 @@ PACKAGING_DIR="$PROJECT_DIR/package"
 
 case "$1" in 
 
-  prepare_upload)
-    cp $PROJECT_DIR/README.md   $PACKAGING_DIR/README.md
-    cp $PROJECT_DIR/csvquery.py $PACKAGING_DIR/csvquery/csvquery.py
-    rm -r $PACKAGING_DIR/dist
-  ;;
-
   upload)
+    # Copy everything into packaging folder
+    cp $PROJECT_DIR/README.md   $PACKAGING_DIR/README.md
+    cp -R $PROJECT_DIR/src/     $PACKAGING_DIR/csvquery
+    [ -d $PACKAGING_DIR ] && 
+      rm -r $PACKAGING_DIR/dist;
+    # Upload the package
     cd $PACKAGING_DIR;
-    rm -r dist/ &&
-      python3 setup.py sdist bdist_wheel &&
+    python3 setup.py sdist bdist_wheel &&
       python3 -m twine upload $PACKAGING_DIR/dist/*
   ;;
 
   *)
     echo "Usage:"
-    echo "  $0 prepare_upload     --->   Copy all the necessary files into the packing directory"
     echo "  $0 upload             --->   CAUTION: Publishes package to PyPi"
-    echo "WARNING: '$0 upload' DOES NOT COPY ALL FILES INTO packaging dir"
   ;;
 
 esac
