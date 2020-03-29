f7d020314e688fb65e3be2fda3e327a9260195b0
AlexDvorak <doe.jimmie@gmail.com>
2020-03-29 16:56:12 -0500

    build script error printing to standard error

--- build/packaging_tools.zsh
+++ build/packaging_tools.zsh
@@ -1,6 +1,15 @@
 #!/usr/bin/env zsh
 
-# update_package_folder <project_root> <package_folder>
+function error_msg() {
+  ([ -z $1 ] || [ -z $2 ]) &&
+    return -1;
+  caller=$1
+  msg=$2
+
+  print "$caller: $msg" 1>&2
+}
+
+# update_pkg_dir <project_root> <package_folder>
 #  0 --> success
 # -1 --> not enough arguments supplied
 function update_pkg_dir() {
@@ -9,9 +18,14 @@ function update_pkg_dir() {
   project=$1
   package=$2
 
-  cp    $project/README.md  $package
-  cp    $project/LICENSE    $package
-  cp -R $project/src/*      $package/csvquery
+  cp    $project/README.md  $package ||
+    error_msg "update_pkg_dir" "failed to copy README.md into package"
+
+  cp    $project/LICENSE    $package ||
+    error_msg "update_pkg_dir" "failed to copy LICENSE into package"
+
+  cp -R $project/src/*      $package/csvquery ||
+    error_msg "update_pkg_dir" "failed to copy src/* into package"
 
   return 0;
 }
@@ -19,25 +33,47 @@ function update_pkg_dir() {
 # clean <package_dir> <package_name>
 #  0 --> success
 # -1 --> not enough arguments supplied
+# -2 --> failed to remove directory
+# -3 --> failed to remove file
 function clean() {
   ([ -z $1 ] || [ -z $2 ]) &&
     return -1;
   pkg_dir=$1
   pkg_name=$2
 
-  [ -d $pkg_dir/dist ] && # package/dist/
-    rm -r $pkg_dir/dist;
+  dirs_to_delete=(
+    "$pkg_dir/dist"
+    "$pkg_dir/build"
+    "$pkg_dir/$pkg_name"
+    "$pkg_dir/$pkg_name.egg-info"
+  )
+  files_to_delete=(
+    "$pkg_dir/README.md"
+    "$pkg_dir/LICENSE"
+  )
 
-  [ -d $pkg_dir/$pkg_name.egg-info ] && # package/my_pkg.egg-info/
-    rm -r $pkg_dir/$pkg_name.egg-info;
+  for dir in $dirs_to_delete; do
+    [ -d $dir ] && (
+      rm -r $dir || (
+        error_msg "clean" "Could not remove directory '$dir'" &&
+        return -2
+      )
+    )
+  done
 
-  [ -d $pkg_dir/build ] && # package/build/
-    rm -r $pkg_dir/build;
+  for file in $files_to_delete; do
+    [ -e $file ] && (
+      rm $file || (
+        error_msg "clean" "Could not remove file '$file'" &&
+        return -3
+      )
+    )
+  done
 
   return 0;
 }
 
-# publish_package <package_folder>
+# publish_pkg <package_folder>
 #  0 --> success
 # -1 --> not enough arguments supplied
 function publish_pkg() {
